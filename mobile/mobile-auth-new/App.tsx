00a17b4a60de56392016910e565c31a7b1d1ecce
lykimq <lykimq@gmail.com>
2025-04-30 12:37:07 +0200

    Fix context in App

--- mobile/mobile-auth-new/App.tsx
+++ mobile/mobile-auth-new/App.tsx
@@ -1,9 +1,13 @@
 import { ExpoRoot } from 'expo-router';
 import { registerRootComponent } from 'expo';
 
+declare const require: {
+  context: (path: string, deep?: boolean, filter?: RegExp) => any;
+};
+
 // Must be exported or Fast Refresh won't update the context
 export function App() {
-  const ctx = require('./app');
+  const ctx = require.context('./app', true, /\.(js|jsx|ts|tsx)$/);
   return <ExpoRoot context={ctx} />;
 }
 
